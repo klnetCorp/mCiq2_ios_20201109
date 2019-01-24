4eac97601e1ccbc09d5371f1b6f23992e2b8455b
juis <juis@juisui-macbook-pro.local>
2019-01-24 13:26:36 +0900

    Initial Commit

---
+++ mCiq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mCiq
+//
+//  Created by juis on 2019. 1. 24..
+//  Copyright © 2019년 juis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mCiq/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  mCiq
+//
+//  Created by juis on 2019. 1. 24..
+//  Copyright © 2019년 juis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
